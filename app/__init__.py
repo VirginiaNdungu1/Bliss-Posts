abe5dfe3313f9c92a1b5ae404a34adad00c5e74e
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-10-31 11:47:54 +0300

    Initiialise flask create app configurations initialise flsk extensions

---
+++ app/__init__.py
@@ -0,0 +1,25 @@
+# initialise flask
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from config import config_options
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+
+
+def create_app(config_option):
+    # initialise application
+    app = Flask(__name__)
+
+    # create app configurations
+    app.config.from_object(config_options[config_option])
+
+    # initialise flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+    # Register Blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
