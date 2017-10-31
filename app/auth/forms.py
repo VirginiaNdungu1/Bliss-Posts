bf16ed7f8aa878a765b0053c7a32ac82a2f1c0b0
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-10-31 12:39:30 +0300

    Minor changes

---
+++ app/auth/forms.py
@@ -0,0 +1,27 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import REquired, EqualTo
+from wtforms import ValidationError
+from ..models import Role, User, Category, Post, Comment
+
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Your Email Address', validators=[Required(), Email()])
+    fullname = StringField('Enter your Full Name', validators=[Required()])
+    username = StringField('Enter your username', validators=[Required()])
+    profession = StringField('Enter your Profession', validators=[Required()])
+    quote = StringField('Life Philosophy', validators=[Required()])
+    # profile_pic
+    user_pwd = PasswordField('Enter Password', validators=[Required(), EqualTo(
+        'user_pwd_confirm', message='Passwords Must Match')])
+    user_pwd_confirm = PasswordField(
+        'Confirm Password', validators=[Required()])
+    submit = SubmitField('Sign Up')
+
+    def validate_email(self, data_field):
+        if User.query.filter_by(email=data_field.data).first():
+            raise ValidationError("There is an account with that email")
+
+    def validate_user(self, data_field):
+        if User.query.filter_by(username=data_field.data).first():
+            raise ValidationError("That username is taken")
