f4c008cadfb47883b99da35a6f92ede37cd367c5
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-10-31 11:51:59 +0300

    Initialise flask class instances Create manager commands create manager shell

---
+++ manage.py
@@ -0,0 +1,33 @@
+from app import create_app, db
+from flask_script import Manager, Server
+from app.models import Role, User, Category, Post, Comment
+from flask_migrate import Migrate, MigrateCommand
+
+
+app = create_app('development')
+
+# app = create_app('test')
+'''
+Create app instance
+How?
+
+Call the create_app function
+pass in the onfiguration_options keys - 'development', 'test'
+'''
+# Initialise flask class instances
+manager = Manager(app)
+
+migrate = Migrate(app, db)
+
+manager.add_command('server', Server)
+# create new manager command and pass in the MigrateCommand class
+manager.add_command('db', MigrateCommand)
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role, Category=Category, Post=Post, Comment=Comment)
+
+
+if __name__ == '__main__':
+    manager.run()
