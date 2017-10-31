689834ed394d9225b1f340951505d3f4c7340e89
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-10-31 12:36:51 +0300

    Add custom validators to ensure one email for each user and one username for each user

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import REquired, EqualTo
+from wtforms import ValidationError
 from ..models import Role, User, Category, Post, Comment
 
 
@@ -10,9 +11,17 @@ class RegistrationForm(FlaskForm):
     username = StringField('Enter your username', validators=[Required()])
     profession = StringField('Enter your Profession', validators=[Required()])
     quote = StringField('Life Philosophy', validators=[Required()])
-    profile_pic
+    # profile_pic
     user_pwd = PasswordField('Enter Password', validators=[Required(), EqualTo(
         'user_pwd_confirm', message='Passwords Must Match')])
     user_pwd_confirm = PasswordField(
         'Confirm Password', validators=[Required()])
     submit = SubmitField('Sign Up')
+
+    def validate_email(self, data_field):
+        if User.query.filter_by(email=data_field.data).first():
+            raise ValidationError("There is an account with that email")
+
+    def validate_user(self, data_field):
+        if User.query.filter_by(username=data_field.data).first():
+            raise ValidationError("That username is taken")
