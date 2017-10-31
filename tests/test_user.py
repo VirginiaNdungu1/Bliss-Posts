270d59bbbce125feeacfc0ead4c948d5057a3da5
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-10-31 22:14:52 +0300

    create test for User Model Class

---
+++ tests/test_user.py
@@ -0,0 +1,17 @@
+import unittest
+from app.models import User
+
+
+class UserModelTest(unittest.TestCase):
+    def setUp(self):
+        self.new_user = User(user_pwd='guava')
+
+    def test_pwd_write_denied(self):
+        with self.assertRaises(AttributeError):
+            self.new_user.password
+
+    def test_set_password(self):
+        self.assertTrue(self.new_user.user_pwd is not None)
+
+    def test_check_password(self):
+        self.assertTrue(self.new_user.verify_password('guava'))
