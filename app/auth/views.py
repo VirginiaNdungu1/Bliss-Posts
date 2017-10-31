3697f161038db155c7ac6d800e271da7c3b50738
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-10-31 15:56:30 +0300

    Update registration view function

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,4 +1,5 @@
-from flask import render_template, redirect, url_for
+from . import auth
+from flask import render_template, redirect, url_for, request
 from ..models import User
 from .forms import RegistrationForm
 from .. import db
@@ -8,9 +9,10 @@ from .. import db
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(email=form.email.data, fullname=form.fullname.data, username=form.username, data, profession=form.profession.data, quote=form.quote.data, user_pwd=form.user_pwd.data)
-        user.session.add(user)
+        user = User(email=form.email.data, fullname=form.fullname.data, username=form.username.data,
+                    profession=form.profession.data, quote=form.quote.data, user_pwd=form.user_pwd.data)
+        db.session.add(user)
         db.session.commit()
-        return render_template(url_for('auth.login'))
-        title = "Bliss Account"
+        return render_template('auth/register.html', registration_form=form)
+    title = "Bliss Account"
     return render_template('auth/register.html', registration_form=form)
