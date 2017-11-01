cebfca2ef07e23e2e022d6e39515e92dcac3f8db
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-11-01 06:44:41 +0300

    update login view function and reate logout view function

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,6 @@
 from . import auth
-from flask_login import login_user
+from .. import main
+from flask_login import login_user, logout_user, login_required, current_user
 from flask import render_template, redirect, url_for, request, flash
 from ..models import User
 from .forms import RegistrationForm, LoginForm
@@ -24,8 +25,20 @@ def login():
     login_form = LoginForm()
     if login_form.validate_on_submit():
         user = User.query.filter_by(email=login_form.email.data).first()
-        if user is not None and user.verify_password(login_form.password.data)
-        return redirect(request.args.get('next') or url_for(main.index))
-        flash("Inavalid Username or Password")
-    title = "Bliss Posts Login"
-    return render_template('auth/login.html', title=title,  login_form=login_form)
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user, login_form.remember.data)
+            flash('successfully logged in')
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('Invalid username or Password')
+
+    title = "Bliss Posts login"
+    return render_template('auth/login.html', login_form=login_form, title=title)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been successfully logged out')
+    return redirect(url_for("main.index"))
